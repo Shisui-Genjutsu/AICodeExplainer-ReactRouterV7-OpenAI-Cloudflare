7d9e3bb870037a7dcb0747683abb49d3ff2302a9
Bhaskar G <bhaskarsd1674@gmail.com>
2025-11-10 20:46:05 +0530

    first commit

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,17 +1,9 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import React from 'react'
 
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+const HomePage = () => {
+  return (
+    <div>Home</div>
+  )
 }
 
-export function loader({ context }: Route.LoaderArgs) {
-  return { message: context.cloudflare.env.VALUE_FROM_CLOUDFLARE };
-}
-
-export default function Home({ loaderData }: Route.ComponentProps) {
-  return <Welcome message={loaderData.message} />;
-}
+export default HomePage
